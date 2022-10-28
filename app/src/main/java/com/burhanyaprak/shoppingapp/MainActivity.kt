0a018f7e2991231b82b01be2f46973623b64e1db
burhanyaprak <burhanyaprak@outlook.com>
2022-10-28 11:47:19 +0300

    Add jetpack navigatiÄon library and addbottom navigation

--- app/src/main/java/com/burhanyaprak/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/burhanyaprak/shoppingapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.burhanyaprak.shoppingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.burhanyaprak.shoppingapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        bottomNavigationView = binding.bottomNavigationView
+        initNavController()
+    }
+
+    private fun initNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
